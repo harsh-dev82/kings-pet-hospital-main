65e24c3e37a1a91ba408cf169aa7b9ba44fa8c4e
harsh-dev82 <darrasingh201@gmail.com>
2025-11-07 17:01:21 +0530

    update the vite.config.js

--- hospital_frontend/vite.config.js
+++ hospital_frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ensures correct routing on Vercel
   plugins: [react(), tailwindcss()]
 })
 
